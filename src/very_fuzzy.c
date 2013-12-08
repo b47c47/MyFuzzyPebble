6ce8d29e96b5930d45567e798e4fb10e09c77c93
Hamish Downer <hamish@foobacca.co.uk>
2013-12-08 18:42:23 +0000

    Get app working with the pebble sdk 2.0

--- src/very_fuzzy.c
+++ src/very_fuzzy.c
@@ -1,63 +1,55 @@
-#include "pebble_os.h"
-#include "pebble_app.h"
-#include "pebble_fonts.h"
+#include <pebble.h>
 #include "num2words.h"
-
-
-#define MY_UUID { 0x3B, 0x38, 0x76, 0x74, 0x3E, 0x98, 0x4A, 0x54, 0xB8, 0x68, 0x1A, 0xE3, 0x16, 0xF0, 0xEA, 0xBC }
-PBL_APP_INFO(MY_UUID,
-             "Very Fuzzy", "foobacca.co.uk",
-             1, 1, /* App version */
-             DEFAULT_MENU_ICON,
-             APP_INFO_WATCH_FACE);
 
 #define BUFFER_SIZE 86
 
 static struct CommonWordsData {
-  TextLayer label;
-  Window window;
+  TextLayer *label;
+  Window *window;
   char buffer[BUFFER_SIZE];
 } s_data;
 
-static void update_time(PblTm* t) {
+static void update_time(struct tm* t) {
   fuzzy_time_to_words(t->tm_hour, t->tm_min, s_data.buffer, BUFFER_SIZE);
-  text_layer_set_text(&s_data.label, s_data.buffer);
+  text_layer_set_text(s_data.label, s_data.buffer);
 }
 
-static void handle_minute_tick(AppContextRef app_ctx, PebbleTickEvent* e) {
-  update_time(e->tick_time);
+static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+  update_time(tick_time);
 }
 
-static void handle_init(AppContextRef ctx) {
-
-  window_init(&s_data.window, "My Fuzzy Time");
+static void do_init(void) {
+  s_data.window = window_create();
   const bool animated = true;
-  window_stack_push(&s_data.window, animated);
+  window_stack_push(s_data.window, animated);
 
-  window_set_background_color(&s_data.window, GColorBlack);
+  window_set_background_color(s_data.window, GColorBlack);
   GFont font = fonts_get_system_font(FONT_KEY_BITHAM_42_LIGHT);
 
-  text_layer_init(&s_data.label, GRect(0, 20, s_data.window.layer.frame.size.w, s_data.window.layer.frame.size.h - 20));
-  text_layer_set_background_color(&s_data.label, GColorBlack);
-  text_layer_set_text_color(&s_data.label, GColorWhite);
-  text_layer_set_font(&s_data.label, font);
-  layer_add_child(&s_data.window.layer, &s_data.label.layer);
+  Layer *root_layer = window_get_root_layer(s_data.window);
+  GRect frame = layer_get_frame(root_layer);
 
-  PblTm t;
-  get_time(&t);
-  update_time(&t);
+  s_data.label = text_layer_create(GRect(0, 20, frame.size.w, frame.size.h - 20));
+  text_layer_set_background_color(s_data.label, GColorBlack);
+  text_layer_set_text_color(s_data.label, GColorWhite);
+  text_layer_set_font(s_data.label, font);
+  layer_add_child(root_layer, text_layer_get_layer(s_data.label));
+
+  time_t now = time(NULL);
+  struct tm *t = localtime(&now);
+  update_time(t);
+
+  tick_timer_service_subscribe(MINUTE_UNIT, &handle_minute_tick);
+}
+
+static void do_deinit(void) {
+  window_destroy(s_data.window);
+  text_layer_destroy(s_data.label);
 }
 
 
-void pbl_main(void *params) {
-  PebbleAppHandlers handlers = {
-    .init_handler = &handle_init,
-
-    .tick_info = {
-      .tick_handler = &handle_minute_tick,
-      .tick_units = MINUTE_UNIT
-    }
-
-  };
-  app_event_loop(params, &handlers);
+int main(void) {
+  do_init();
+  app_event_loop();
+  do_deinit();
 }
